e5d3a2fef7027b118505574d63d48df3816bf9ab
Ammar Faouri <eng.ammarfaouri@gmail.com>
2020-06-24 16:12:51 +0300

    Added axios to recipes, Added Minirecipe component

--- react-client/src/Recipes.js
+++ react-client/src/Recipes.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import MiniRecipe from "./MiniRecipe";
+import CardDeck from "react-bootstrap/CardDeck";
+import axios from "axios";
 
 class Recipes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("/recipes")
+      .then((response) => this.setState({ recipes: response.data }))
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
   render() {
-    return <div className="Recipes">Recipes</div>;
+    let miniRecipes = this.state.recipes.map((recipe) => (
+      <MiniRecipe recipe={recipe} />
+    ));
+    return (
+      <div className="Recipes">
+        <CardDeck>{miniRecipes}</CardDeck>
+      </div>
+    );
   }
 }
 
