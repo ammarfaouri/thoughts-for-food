8840d03b9ea81dea9ab2f65608377c0fa125b465
Ammar Faouri <eng.ammarfaouri@gmail.com>
2020-06-26 17:04:20 +0300

    Updated Minirecipe to show some recipe info

--- react-client/src/MiniRecipe.js
+++ react-client/src/MiniRecipe.js
@@ -1,22 +1,19 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 class MiniRecipe extends Component {
   render() {
+    let { name, author, description, prepTime, difficulty } = this.props.recipe;
     return (
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
+      <Card className="mt-5">
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
+          <Card.Title>{name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">By {author}</Card.Subtitle>
+          <Card.Text>{description.substring(0, 100)}...</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
         </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
     );
   }
