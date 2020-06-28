222b18d469f76994834967955439cbc0007da35a
Ammar Faouri <eng.ammarfaouri@gmail.com>
2020-06-28 22:06:13 +0300

    Updated SingleRecipe to get data from API

--- react-client/src/SingleRecipe.js
+++ react-client/src/SingleRecipe.js
@@ -1,11 +1,69 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class SingleRecipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      author: "",
+      description: "",
+      prepTime: "",
+      difficulty: "",
+      ingredients: [],
+      method: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(`/recipes/${this.props.match.params.id}`)
+      .then((response) => {
+        let { data } = response;
+        this.setState({
+          name: data.name,
+          author: data.author,
+          description: data.description,
+          prepTime: data.prepTime,
+          difficulty: data.difficulty,
+          ingredients: data.ingredients,
+          method: data.method,
+        });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
   render() {
+    let {
+      name,
+      author,
+      description,
+      prepTime,
+      difficulty,
+      ingredients,
+      method,
+    } = this.state;
+    let ingredientList = ingredients.map((ingredient) => {
+      return (
+        <li>
+          {ingredient.amount},{ingredient.unit},{ingredient.name}
+        </li>
+      );
+    });
+    let methodList = method.map((step) => {
+      return <li>{step}</li>;
+    });
+
     return (
       <div className="SingleRecipe">
-        {this.props.id}
-        Single recipe! {this.props.match.params.id}
+        <h3>{name}</h3>
+        <h3>{author}</h3>
+        <h3>{description}</h3>
+        <h3>{prepTime}</h3>
+        <h3>{difficulty}</h3>
+        <ul>{ingredientList}</ul>
+        <ui>{methodList}</ui>
       </div>
     );
   }
