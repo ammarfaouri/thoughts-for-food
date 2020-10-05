12c7659ad388e139bb960b01db32a0ac19c94b57
Ammar Faouri <eng.ammarfaouri@gmail.com>
2020-10-05 12:52:04 +0300

    Added styling for About page

--- react-client/src/About.js
+++ react-client/src/About.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function About() {
-  return <div className="About">ABOUT</div>;
+  return (
+    <div className="About">
+      <h2>What is this website?</h2>
+      <img
+        className="aboutimg"
+        src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+      />
+      <p>
+        Our Website is a one-stop for all your food needs. You can view our
+        collection of recipes and you can create your own and share with your
+        friends. we've got everything you need.
+      </p>
+    </div>
+  );
 }
 
 export default About;
